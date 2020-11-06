798f4ddd35cd7998173fe25a56fdaf1a9ae3ab66
thibault rahou <trahou33@gmail.com>
2020-11-06 15:06:22 +0100

    product controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database'
+
+Product.destroy_all
+
+puts 'Create products'
+
+Product.new(title: "Sporting Casino", description:"color", quantity:"14",photo: "casino.png", size: "M", category: "t-shirt").save
+Product.new(title: "Hotel Mercedes", description:"color", quantity:"14", photo: "mercedes.png", size: "L", category: "t-shirt").save
+Product.new(title: "Place des Landais", description:"color", quantity:"14", photo: "place.png", size: "S", category: "t-shirt").save
