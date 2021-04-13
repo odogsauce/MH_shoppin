b7527f734b6bf597dc624c0cb935a8699aa8f76c
thibault rahou <trahou33@gmail.com>
2021-04-13 16:48:14 +0200

    livraison&contact

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
 root 'products#index'
+  get 'contact', to: 'pages#contact', as: :contact
+  get 'livraison', to: 'pages#livraison', as: :livraison
 resources :products, only: [:index, :show]
 
 resources :orders, only: [:create, :show] do
